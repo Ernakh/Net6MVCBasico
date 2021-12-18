0753a6635e21cd9a6275c15eabab8e694b2d6125
Ernakh <fabriciotonettolondero@gmail.com>
2021-12-18 17:12:57 -0300

    first commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Net6MVCBasico.Models;
+
+namespace Net6MVCBasico.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Sobre()
+        {
+            return View();
+        }
+
+        //public IActionResult Pessoa()
+        //{
+        //    Pessoa p = new Pessoa();
+        //    p.Nome = "Fabrício Tonetto Londero";
+        //    return View(p);
+        //}
+
+        public IActionResult Pessoa(Pessoa p)
+        {
+            return View(p);
+        }
+
+        [HttpPost]
+        public IActionResult Cadastrar(Pessoa p)
+        {
+            string nome = p.Nome;
+
+            return View("Pessoa", p);
+        }
+
+        public IActionResult Cadastrar()
+        {
+            return View();
+        }
+    }
+}
---
+++ Models/Pessoa.cs
@@ -0,0 +1,7 @@
+﻿namespace Net6MVCBasico.Models
+{
+    public class Pessoa
+    {
+        public string Nome { get; set; }
+    }
+}
